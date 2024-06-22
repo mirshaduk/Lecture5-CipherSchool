c579db45ac4f24ed844ae3255049dc28065f072a
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-06-22 11:45:42 +0530

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{ 
+    //Execute AND and OR statements and print out the boolean :
+
+
+    //Execute if else statement :
+
+
+    // switch case conditional statement: 
+
+    
+    int a,b;
+    int c,d;
+
+
+    cout<<"input 2 integers a,b,c and d" <<endl;
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    //statement : a>b
+    cout<<(a>b || c>d);
+    //c++ interpreters true as 1
+    //c++ interpreters false as 0
+      
+
+    cout<<(a>b);
+
+
+}
